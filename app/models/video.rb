c6a3754c026a55cacd3f7f3842079f20c9b7d9df
Toby Woller <toby@woller.io>
2015-06-02 09:53:14 -0700

    Implement 'search_by_title' on the video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,10 @@ class Video < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
+
+  def self.search_by_title(search_term)
+    return [] if search_term.blank?
+    video = Video.arel_table
+    Video.where(video[:title].matches("%#{search_term}%")).order(created_at: :desc)
+  end
 end
