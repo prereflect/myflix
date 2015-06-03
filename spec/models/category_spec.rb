58c29e1fd6384c5e9937a05e77f51cbf3ce466a9
Toby Woller <toby@woller.io>
2015-06-03 11:26:06 -0700

    Failure/Error ActiveRecord::AssociationTypeMismatch:
    Failure/Error: skate = Video.create(title: 'Skate or Die',
    description: 'Not really', category: 'sports', created_at: 1.day.ago)
    ActiveRecord::AssociationTypeMismatch:
      Category(#70365159901180) expected, got String(#70365128772400)

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,15 @@ describe Category do
   it { should have_many(:videos).order('title') }
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+
+  describe '#recent_videos' do
+    it 'returns the videos in the reverse chronological order' do
+      sports = Category.create(name: 'sports')
+
+      skate = Video.create(title: 'Skate or Die', description: 'Not really', category: 'sports', created_at: 1.day.ago)
+      snowboard = Video.create(title: 'Snowboard or Die', description: 'Not really', category: 'sports')
+
+      expect(sports.recent_videos).to eq([snowboard, skate])
+    end
+  end
 end
