7288b0cbefe79a7a10040bdc37f36f94223761c3
Toby Woller <toby@woller.io>
2015-06-10 09:12:39 -0700

    Add Front, Register, and Sign In Pages
    Add Controllers for pages, sessions, and users
    
    Add users model
    
    Add views
      pages#front
      sessions#new
      users#new
    
    Add user_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Myflix::Application.routes.draw do
+  root to: 'pages#front'
+
   get 'home', to: 'videos#index'
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
 
   resources :videos, only: [:index, :show] do
     collection do
       get :search, to: 'videos#search'
     end
   end
-  resources :categories, only: :show
+  resources :categories, only: [:show]
+  resources :users, only: [:create]
 end
