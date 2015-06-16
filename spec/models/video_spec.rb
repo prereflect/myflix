66e31a8de9ebc3512bd5ce1a1f60df66d639394f
Toby Woller <toby@woller.io>
2015-06-16 15:37:01 -0700

    Suggested Changes
    @current_user ||= # Set instance variable and memoize
    before_filter => before_action
    No mixing Haml and Rails helper tags
    Realign bootstrap form for attributes
    Fix Rspec Describe

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,30 +5,28 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
-  describe 'video search by attribute' do
+  describe '.search_by_title' do
     let!(:dragon) { Video.create(title: 'How to Train Your Dragon', description: 'Hiccup and Toothless, BFF') }
     let(:cop) { Video.create(title: 'Training Day', description: 'Bad Cop, Good Cop') }
 
-    describe '.search_by_title' do
-      it 'returns an empty array if there is no match' do
-        expect(Video.search_by_title('hello')).to eq([])
-      end
+    it 'returns an empty array if there is no match' do
+      expect(Video.search_by_title('hello')).to eq([])
+    end
 
-      it 'returns an empty array with a search for an empty string' do
-        expect(Video.search_by_title('')).to eq([])
-      end
+    it 'returns an empty array with a search for an empty string' do
+      expect(Video.search_by_title('')).to eq([])
+    end
 
-      it 'returns an array of one video for an exact match' do
-        expect(Video.search_by_title('How to Train Your Dragon')).to eq([dragon])
-      end
+    it 'returns an array of one video for an exact match' do
+      expect(Video.search_by_title('How to Train Your Dragon')).to eq([dragon])
+    end
 
-      it 'returns an array of one video for a partial match' do
-        expect(Video.search_by_title('how')).to eq([dragon])
-      end
+    it 'returns an array of one video for a partial match' do
+      expect(Video.search_by_title('how')).to eq([dragon])
+    end
 
-      it 'returns an array of all matches ordered by created_at desc' do
-        expect(Video.search_by_title('train')).to eq([cop, dragon])
-      end
+    it 'returns an array of all matches ordered by created_at desc' do
+      expect(Video.search_by_title('train')).to eq([cop, dragon])
     end
   end
 end
