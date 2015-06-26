867d2e731da45f7ce1379520ee4e4a2b7d275c44
Toby Woller <toby@woller.io>
2015-06-26 03:00:45 -0700

    Add Flash Notices to Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,12 @@ class  ReviewsController < ApplicationController
   def create
     @video = Video.find(params[:video_id])
     review = @video.reviews.create(review_params.merge!(user: current_user))
+
     if review.save
+      flash[:success] = 'Your review has been created'
       redirect_to @video
     else
+      flash[:danger] = 'Please complete your review'
       @reviews = @video.reviews.reload
       render 'videos/show'
     end
